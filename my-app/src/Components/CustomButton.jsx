ba25f6c6d5fcda80ed1f64a12d7e93901d1e57d0
Alisafa13 <e.l_sefa@icloud.com>
2023-11-24 15:15:48 +0400

    update

--- my-app/src/Components/CustomButton.jsx
+++ my-app/src/Components/CustomButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
+import "./CustomButton.css"
 
 const CustomButton = () => {
 
@@ -11,9 +12,12 @@ const CustomButton = () => {
     };
     const toggleWarnMessage = () => {
        alert("Warning!")
+       let mess = document.querySelector('.warning').style.color = "red";
+ 
     };
     const toggleHint = () => {
         let hover = document.querySelector(".hover").innerHTML = "Heyatda ugurlar!";
+        
     };
     const toggleHint2 = () => {
         let hover = document.querySelector(".hover").innerHTML = "";
@@ -25,7 +29,7 @@ const CustomButton = () => {
             {text && <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, dicta!</h2>}
             <br />
             <br />
-            <button onClick={toggleWarnMessage}>warnMessage</button>
+            <button className="warning" onClick={toggleWarnMessage}>warnMessage</button>
             <br />
             <br />
             <button onMouseOver={toggleHint} onMouseLeave={toggleHint2}>hint</button>
